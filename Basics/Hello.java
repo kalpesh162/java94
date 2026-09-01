35f77c1e34097a69af60672923a9cde87c5f5d7f
kalpesh <kalpesh162@gmail.com>
2026-09-01 08:21:09 +0530

    Initial commit

---
+++ Basics/Hello.java
@@ -0,0 +1,52 @@
+/*
+JDK INSTALLATION --->  C:\Program Files\Java\jdk1.8.0_241
+
+YOUR APP SOURCE CODE ---> E:\JAVA-WorkSpace-JAP-94\Basics\
+														|
+														|
+														|--- name.java
+														|
+
+How to run code Open Command Promt
+C:/User/name
+   |
+Change Drive
+  |
+E:
+  |
+Change Directory
+  |
+E: > cd JAVA-WorkSpace-JAP-94
+  |
+E:/JAVA-WorkSpace-JAP-94> cd Basics
+  |
+E:\JAVA-WorkSpace-JAP-94\Basics\
+  |
+set path="C:\Program Files\Java\jdk1.8.0_241\bin"
+  |
+javac Hello.java
+  |
+java Hello
+
+*/
+
+// Source Code
+// () Parenthesis
+// main()  Method | Function 
+// Entry Gate
+//[] subscript
+// String args[]  Array
+
+// { } flower Brackets
+class Hello{
+
+public  static void main(String args[]){
+	int age=22;
+	System.out.print(age);
+	System.out.println("Hello");
+	System.out.println("Kalpesh");
+	System.out.println(55.555);
+
+}
+
+}
